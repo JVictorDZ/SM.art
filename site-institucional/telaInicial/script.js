86a628fb53b992ed4d119a26b6a33cf8af46b38a
Raul Reis <raulgomesreis@gmail.com>
2024-04-19 12:44:11 -0300

    Troca do if por switch na função rota

--- site-institucional/telaInicial/script.js
+++ site-institucional/telaInicial/script.js
@@ -1,10 +1,19 @@
 function rota(valor){
   // alert(valor);
-  if(valor == 'simulador'){
-    window.location.href = '../Calculadora-Financeira/calc.html';
-  } else if (valor == 'login'){
-    window.location.href = '../LoginCadastro/login.html';
-  } else if(valor == 'cadastro'){
-    window.location.href = '../LoginCadastro/index.html';
+  switch (valor) {
+    case 'simulador':
+      window.location.href = '../Calculadora-Financeira/calc.html';
+      break;
+    case 'login':
+      window.location.href = '../LoginCadastro/login.html';
+      break;
+    case 'cadastro':
+      window.location.href = '../LoginCadastro/index.html';
+      break;
+    case 'sobrenos':
+      window.location.href = '../telaSobreNos/index.html';
+    default:
+      alert('Nenhuma página com essa referência encontrada!')
+      break;
   }
 }
