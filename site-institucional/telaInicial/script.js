c7698293e431d633102b8163c225d94dc547ff14
Raul Reis <raulgomesreis@gmail.com>
2024-04-20 15:09:45 -0300

    Troca do if por switch na função rota

--- site-institucional/telaInicial/script.js
+++ site-institucional/telaInicial/script.js
@@ -1,18 +1,19 @@
 function rota(valor){
-  // alert(valor);
-  switch (valor) {
-    case 'simulador':
+  // switch é similar ao if, ele tem um parâmetro de variável onde fara as verificações em cada caso, denominado por "case".
+
+  switch (valor) { // Váriavel de caso
+    case 'simulador': // Caso valor == 'simulador' execute
       window.location.href = '../Calculadora-Financeira/calc.html';
-      break;
+      break; // ordem de parada
     case 'login':
       window.location.href = '../LoginCadastro/login.html';
       break;
-    case 'cadastro':
+    case 'cadastro': // Caso valor == 'cadastro' execute
       window.location.href = '../LoginCadastro/index.html';
-      break;
+      break; // ordem de parada
     case 'sobrenos':
       window.location.href = '../telaSobreNos/index.html';
-    default:
+    default: // default é similar ao else, ou seja, se a variável valor não se enquadrar em nenhum dos casos execute...
       alert('Nenhuma página com essa referência encontrada!')
       break;
   }
