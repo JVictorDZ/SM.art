da054b3e61d9bb6fcbc7141b570d48ff42069225
Raul Reis <raulgomesreis@gmail.com>
2024-04-24 21:44:05 -0300

    Seção sobre nós adicionada

--- site-institucional/telaInicial/script.js
+++ site-institucional/telaInicial/script.js
@@ -1,20 +1,15 @@
 function rota(valor){
   // switch é similar ao if, ele tem um parâmetro de variável onde fara as verificações em cada caso, denominado por "case".
 
-  switch (valor) { // Váriavel de caso
-    case 'simulador': // Caso valor == 'simulador' execute
-      window.location.href = '../Calculadora-Financeira/calc.html';
-      break; // ordem de parada
-    case 'login':
-      window.location.href = '../LoginCadastro/login.html';
-      break;
-    case 'cadastro': // Caso valor == 'cadastro' execute
-      window.location.href = '../LoginCadastro/index.html';
-      break; // ordem de parada
-    case 'sobrenos':
-      window.location.href = '../telaSobreNos/index.html';
-    default: // default é similar ao else, ou seja, se a variável valor não se enquadrar em nenhum dos casos execute...
-      alert('Nenhuma página com essa referência encontrada!')
-      break;
+  if(valor == 'simulador'){
+    window.location.href = '../Calculadora-Financeira/calc.html';
+  } else if(valor == 'login'){
+    window.location.href = '../LoginCadastro/login.html';
+  } else if(valor == 'cadastro'){
+    window.location.href = '../LoginCadastro/index.html';
+  } else if('sobrenos'){
+    window.location.href = '../telaSobreNos/index.html';
+  } else{
+    alert('Nenhuma página com essa referência encontrada!');
   }
 }
