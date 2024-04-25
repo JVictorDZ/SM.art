dc46d4f8fe43b3dd9f75c17e228807ca19630e8e
JVictorDZ <joao.silva998@sptech.school>
2024-04-25 18:21:19 -0300

    DashBoard

---
+++ site-institucional/DashBoard/script.js
@@ -0,0 +1,12 @@
+function rota(valor){
+  // alert(valor);
+  if(valor == 'simulador'){
+    window.location.href = '../Calculadora-Financeira/calc.html';
+  } else if (valor == 'login'){
+    window.location.href = '../LoginCadastro/login.html';
+  } else if(valor == 'cadastro'){
+    window.location.href = '../LoginCadastro/index.html';
+  } else if (valor == 'telaInicial'){
+    window.location.href = '../telaInicial/index.html';
+  }
+}
