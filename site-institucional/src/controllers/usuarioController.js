6ba8ed2baae9d684e2137306914f087fea41bcc7
EnzoBizeli <enzo.bizeli@sptech.school>
2024-06-07 13:51:47 -0300

    Criptografia implementada

--- site-institucional/src/controllers/usuarioController.js
+++ site-institucional/src/controllers/usuarioController.js
@@ -1,10 +1,13 @@
 var usuarioModel = require("../models/usuarioModel");
 
 function cadastrar(req, res) {
-    var nome = req.body.nomeServer;  // Ajustado para corresponder ao campo correto
-    var email = req.body.emailServer; // Ajustado para corresponder ao campo correto
-    var senha = req.body.senhaServer; // Ajustado para corresponder ao campo correto
-    var cpf = req.body.cpfServer; // Ajustado para corresponder ao campo correto
+    let nome = req.body.nomeServer;  // Ajustado para corresponder ao campo correto
+    let email = req.body.emailServer; // Ajustado para corresponder ao campo correto
+    let senha = req.body.senhaServer; // Ajustado para corresponder ao campo correto
+    let cpf = req.body.cpfServer; // Ajustado para corresponder ao campo correto
+
+    senha = criptografia(senha);
+
 
     usuarioModel.cadastrar(nome, email, senha, cpf)
         .then(function (resultado) {
@@ -17,8 +20,9 @@ function cadastrar(req, res) {
 }
 
 function autenticar(req, res) {
-    var email = req.body.emailServer; // Corrigido
-    var senha = req.body.senhaServer; // Corrigido
+    let email = req.body.emailServer; // Corrigido
+    let senha = req.body.senhaServer; // Corrigido
+    senha = criptografia(senha);
 
     usuarioModel.autenticar(email, senha)
         .then(function (resultado) {
@@ -60,6 +64,42 @@ function obterSalasESensores(req, res) {
         });
 }
 
+function criptografia(senha) {
+    let firstl = ['a', 'e', 'i', 'o', 'u'];
+    let veriV = 0;
+
+    senha = senha.toLowerCase();
+
+    for (let i = 0; i < firstl.length; i++) {
+        veriV = senha.indexOf(firstl[i]);
+        
+        if (veriV == 0) {
+            senha = senha.replace(firstl[i], '#Vf');
+        } 
+        if (veriV == 1) {
+            senha = senha.replace(firstl[i], '12');
+        } 
+        if (veriV == 2) {
+            senha = senha.replace(firstl[i], 'p7');
+        } 
+        if (veriV == 3) {
+            senha = senha.replace(firstl[i], '[r*');
+        } 
+        if(veriV == senha.length - 1){
+            senha = senha.replace(firstl[i], '#w!');
+        }
+        if(veriV == senha.length - 2){
+            senha = senha.replace(firstl[i], '+0!');
+        }
+        if(veriV == senha.length - 3){
+            senha = senha.replace(firstl[i], '76');
+        }
+
+        console.log('\n\nSenha com criptografia: ' + senha)
+    }
+
+    return senha;
+}
 
 module.exports = {
     cadastrar,
