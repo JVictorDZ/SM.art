65cb423d89971ac9aeb8b5173bfc549cd0927c54
JVictorDZ <joao.silva998@sptech.school>
2024-06-12 11:39:54 -0300

    BobIA implementada

--- site-institucional/app.js
+++ site-institucional/app.js
@@ -7,12 +7,23 @@ var ambiente_processo = "desenvolvimento";
 var caminho_env = ambiente_processo === "producao" ? ".env" : ".env.dev";
 dotenv.config({ path: caminho_env });
 
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+
+
 var PORTA_APP = process.env.APP_PORT || 3000;
 var HOST_APP = process.env.APP_HOST || 'localhost';
 
 var app = express();
 
 app.use(express.static(path.join(__dirname, 'telaInicial')));
+
+// configurando o servidor express
+const PORTA_SERVIDOR = process.env.PORTA;
+
+// configurando o gemini (IA)
+const chatIA = new GoogleGenerativeAI(process.env.MINHA_CHAVE);
+
+
 
 var indexRouter = require("./src/routes/index");
 var usuarioRouter = require("./src/routes/usuarios");
@@ -23,12 +34,14 @@ var empresasRouter = require("./src/routes/empresas");
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public', 'telaInicial')));
 
 app.use(cors());
 
 app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, 'telaInicial', 'index.html'));
+  res.sendFile(path.join(__dirname,'public', 'telaInicial', 'index.html'));
 });
+
 
 app.use("/usuarios", usuarioRouter);
 app.use("/avisos", avisosRouter);
@@ -37,13 +50,13 @@ app.use("/empresas", empresasRouter);
 
 app.listen(PORTA_APP, function () {
   console.log(`
-    ##   ##  ######   #####             ####       ##     ######     ##              ##  ##    ####    ######  
-    ##   ##  ##       ##  ##            ## ##     ####      ##      ####             ##  ##     ##         ##  
-    ##   ##  ##       ##  ##            ##  ##   ##  ##     ##     ##  ##            ##  ##     ##        ##   
-    ## # ##  ####     #####    ######   ##  ##   ######     ##     ######   ######   ##  ##     ##       ##    
-    #######  ##       ##  ##            ##  ##   ##  ##     ##     ##  ##            ##  ##     ##      ##     
-    ### ###  ##       ##  ##            ## ##    ##  ##     ##     ##  ##             ####      ##     ##      
-    ##   ##  ######   #####             ####     ##  ##     ##     ##  ##              ##      ####    ######  
+  
+░██████╗███╗░░░███╗░░░░█████╗░██████╗░████████╗
+██╔════╝████╗░████║░░░██╔══██╗██╔══██╗╚══██╔══╝
+╚█████╗░██╔████╔██║░░░███████║██████╔╝░░░██║░░░
+░╚═══██╗██║╚██╔╝██║░░░██╔══██║██╔══██╗░░░██║░░░
+██████╔╝██║░╚═╝░██║██╗██║░░██║██║░░██║░░░██║░░░
+╚═════╝░╚═╝░░░░░╚═╝╚═╝╚═╝░░╚═╝╚═╝░░╚═╝░░░╚═╝░░░
     \n\n\n                                                                                                 
     Servidor do seu site já está rodando! Acesse o caminho a seguir para visualizar .: http://${HOST_APP}:${PORTA_APP} :. \n\n
     Você está rodando sua aplicação em ambiente de .:${process.env.AMBIENTE_PROCESSO}:. \n\n
@@ -51,3 +64,33 @@ app.listen(PORTA_APP, function () {
     \tSe .:producao:. você está se conectando ao banco remoto. \n\n
     \t\tPara alterar o ambiente, comente ou descomente as linhas 1 ou 2 no arquivo 'app.js'\n\n`);
 });
+
+app.post("/perguntar", async (req, res) => {
+  const pergunta = req.body.pergunta;
+
+  try {
+      const resultado = await gerarResposta(pergunta);
+      res.json( { resultado } );
+  } catch (error) {
+      res.status(500).json({ error: 'Erro interno do servidor' });
+  }
+
+});
+
+async function gerarResposta(mensagem) {
+  // obtendo o modelo de IA
+  const modeloIA = chatIA.getGenerativeModel({ model: "gemini-pro" });
+
+  try {
+      // gerando conteúdo com base na pergunta
+      const resultado = await modeloIA.generateContent(`Em um paragráfo responda: ${mensagem}`);
+      const resposta = await resultado.response.text();
+      
+      console.log(resposta);
+
+      return resposta;
+  } catch (error) {
+      console.error(error);
+      throw error;
+  }
+}
